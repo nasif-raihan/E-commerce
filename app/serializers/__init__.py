eaacb8b4f87dbdb9d66f2cf8cab9f1a5387615c3
nasif raihan <nasif.raihan78@gmail.com>
2025-03-02 12:34:59 +0600

    added serializers

---
+++ app/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .cart_serializer import CartSerializer
+from .customer_serializer import CustomerSerializer
+from .order_serializer import OrderSerializer
+from .product_serializer import ProductSerializer
