d45ff3a9bbde819ecfa199645bfdbc56300436fb
nasif raihan <nasif.raihan78@gmail.com>
2025-03-02 16:36:32 +0600

    added urls

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+PAGE_SIZE = 10
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",  # Usual Django login
