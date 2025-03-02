90208a3dfb08f7dced27317b135b355859bc253d
nasif raihan <nasif.raihan78@gmail.com>
2025-03-02 16:38:37 +0600

    added urls

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import CartViewSet
+from .viewsets import CartViewSet, CustomerViewSet, OrderViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r"carts", CartViewSet, basename="cart")
-urlpatterns = [
-    path("", include(router.urls))
-]
+router.register(r"customers", CustomerViewSet, basename="customer")
+router.register(r"orders", OrderViewSet, basename="order")
+router.register(r"products", ProductViewSet, basename="product")
+
+urlpatterns = [path("", include(router.urls))]
