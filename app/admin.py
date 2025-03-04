aa452c40eb8f7fae395c690399a79b219cc7e725
nasif raihan <nasif.raihan78@gmail.com>
2025-03-04 10:51:39 +0600

    models registered in the admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, Customer, Product, Order
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "name", "city", "locality", "zipcode", "division")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "category",
+        "brand",
+        "selling_price",
+        "discounted_price",
+        "product_image",
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "quantity",
+        "ordered_date",
+        "status",
+        "user",
+        "customer",
+        "product",
+    )
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "quantity", "product")
