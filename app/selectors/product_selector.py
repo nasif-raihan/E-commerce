09dfb11138be5bf4ea939e90af0131d790a7be27
nasif raihan <nasif.raihan78@gmail.com>
2025-05-06 22:53:24 +0600

    added ProductSelector's dynamic filters

--- app/selectors/product_selector.py
+++ app/selectors/product_selector.py
@@ -1,10 +1,20 @@
 from django.shortcuts import get_object_or_404
 
-from ..utils import BaseSelector
 from ..models import Product
+from ..utils import BaseSelector
 
 
 class ProductSelector(BaseSelector):
+    fields_map = {
+        "category": "category",
+        "brand": "brand__icontains",
+        "title": "title__icontains",
+        "selling_price_low": "selling_price__gte",
+        "selling_price_high": "selling_price__lte",
+        "discounted_price_low": "discounted_price__gte",
+        "discounted_price_high": "discounted_price__lte",
+    }
+
     @staticmethod
     def retrieve(pk: int) -> Product:
         return get_object_or_404(Product, pk=pk)
